405adc37e3bba9fe1cf8f0793a0b3bba36584f18
M Haidar Hanif <git@mhaidarhanif.com>
2017-11-07 11:27:40 +0700

    Plan out questions data

--- web/src/data/questions.js
+++ web/src/data/questions.js
@@ -1,14 +1,64 @@
 module.exports = [
   {
     id: 1,
-    title: "Always happy?"
+    title: "Always happy?",
+    createdBy: {
+      _id: 1,
+      name: "M Haidar Hanif"
+    },
+    answers: [
+      {
+        _id: 0,
+        text: "The answer we have been looking for",
+        createdBy: {
+          _id: 0,
+          name: "Admin"
+        }
+      },
+      {
+        _id: 1,
+        text: "Another cool answer we are waiting",
+        createdBy: {
+          _id: 1,
+          name: "M Haidar Hanif"
+        }
+      }
+    ]
   },
+
   {
     id: 2,
-    title: "Become hungry?"
+    title: "Become hungry?",
+    createdBy: {
+      _id: 3,
+      name: "Other User"
+    },
+    answers: [
+      {
+        _id: 0,
+        text: "The best answer",
+        createdBy: {
+          _id: 0,
+          name: "Admin"
+        }
+      },
+      {
+        _id: 1,
+        text: "Some useful answers",
+        createdBy: {
+          _id: 3,
+          name: "Other User"
+        }
+      }
+    ]
   },
+
   {
     id: 3,
-    title: "Cooling off?"
+    title: "Cooling off?",
+    createdBy: {
+      _id: 0,
+      name: "M Haidar Hanif"
+    }
   }
 ]
