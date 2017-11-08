c44f793f1cab906d8586d365e9e1b3d46f19c11b
M Haidar Hanif <git@mhaidarhanif.com>
2017-11-08 20:01:46 +0700

    Integrate from Web to API

--- web/src/organisms/ColumnLogin.js
+++ web/src/organisms/ColumnLogin.js
@@ -1,26 +1,79 @@
 import React from "react"
-import { Col, Button, Form, FormGroup, Label, Input } from "reactstrap"
+import { Col, Form, FormGroup, Label, Input } from "reactstrap"
+import axios from "axios"
 
 // import Link from "../atoms/Link"
 
-const ColumnLogin = () => (
-  <Col xs={6}>
-    <h3>Login</h3>
-    <Form>
-      <FormGroup>
-        <Label for="loginEmail">Email</Label>
-        <Input type="email" name="loginEmail" id="loginEmail" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="loginPassword">Password</Label>
-        <Input type="password" name="loginPassword" id="loginPassword" />
-      </FormGroup>
-      {/* <FormGroup>
-        <Link to="/forgot">Forgot password?</Link>
-      </FormGroup> */}
-      <Button>Login</Button>
-    </Form>
-  </Col>
-)
+export default class ColumnLogin extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-export default ColumnLogin
+  handleChange = event => {
+    const name = event.target.name
+    const value = event.target.value
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    const payload = {
+      email: this.state.loginEmail,
+      password: this.state.loginPassword
+    }
+
+    axios
+      .post(`http://localhost:1337/auth/login`, payload)
+      .then(response => {
+        console.log(response.data)
+
+        alert(`SUCCESS! ${JSON.stringify(response.data)}`)
+
+        window.localStorage.setItem("user", response.data)
+
+        // EMPTY OUT FORM INPUTS
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    return (
+      <Col xs={6}>
+        <h3>Login</h3>
+        <Form onSubmit={this.handleSubmit}>
+          <FormGroup>
+            <Label for="loginEmail">Email</Label>
+            <Input
+              type="email"
+              name="loginEmail"
+              id="loginEmail"
+              value={this.state.loginEmail}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="loginPassword">Password</Label>
+            <Input
+              type="password"
+              name="loginPassword"
+              id="loginPassword"
+              value={this.state.loginPassword}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          {/* <FormGroup>
+            <Link to="/forgot">Forgot password?</Link>
+          </FormGroup> */}
+          <input type="submit" value="Login" />
+        </Form>
+      </Col>
+    )
+  }
+}
--- web/src/organisms/ColumnSignup.js
+++ web/src/organisms/ColumnSignup.js
@@ -1,25 +1,81 @@
 import React from "react"
-import { Col, Button, Form, FormGroup, Label, Input } from "reactstrap"
+import { Col, Form, FormGroup, Label, Input } from "reactstrap"
+import axios from "axios"
 
-const ColumnSignup = () => (
-  <Col xs={6}>
-    <h3>Sign Up</h3>
-    <Form>
-      <FormGroup>
-        <Label for="signupFullName">Full Name</Label>
-        <Input type="email" name="signupFullName" id="signupFullName" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="signupEmail">Email</Label>
-        <Input type="email" name="signupEmail" id="signupEmail" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="signupPassword">Password</Label>
-        <Input type="password" name="signupPassword" id="signupPassword" />
-      </FormGroup>
-      <Button>Sign Up</Button>
-    </Form>
-  </Col>
-)
+export default class ColumnSignup extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-export default ColumnSignup
+  handleChange = event => {
+    const name = event.target.name
+    const value = event.target.value
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    const payload = {
+      name: this.state.signupName,
+      email: this.state.signupEmail,
+      password: this.state.signupPassword
+    }
+
+    axios
+      .post(`http://localhost:1337/auth/signup`, payload)
+      .then(response => {
+        console.log(response.data)
+        alert(`SUCCESS! ${JSON.stringify(response.data)}`)
+        // EMPTY OUT FORM INPUTS
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    return (
+      <Col xs={6}>
+        <h3>Sign Up</h3>
+        <Form onSubmit={this.handleSubmit}>
+          <FormGroup>
+            <Label for="signupName">Full Name</Label>
+            <Input
+              type="text"
+              name="signupName"
+              id="signupName"
+              value={this.state.signupName}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="signupEmail">Email</Label>
+            <Input
+              type="email"
+              name="signupEmail"
+              id="signupEmail"
+              value={this.state.signupEmail}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="signupPassword">Password</Label>
+            <Input
+              type="password"
+              name="signupPassword"
+              id="signupPassword"
+              value={this.state.signupPassword}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <input type="submit" value="Sign Up" />
+        </Form>
+      </Col>
+    )
+  }
+}
