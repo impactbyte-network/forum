62faa34a53ba31d565146e4af17101fbb7127c3f
M Haidar Hanif <git@mhaidarhanif.com>
2017-11-08 23:14:40 +0700

    Implement basic get users API

--- api/app.js
+++ api/app.js
@@ -31,8 +31,8 @@ app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/", index)
 app.use("/auth", auth)
-app.use("/users", users)
-app.use("/questions", questions)
+app.use("/api/users", users)
+app.use("/api/questions", questions)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- api/routes/users/controller.js
+++ api/routes/users/controller.js
@@ -1,11 +1,22 @@
+const User = require("../users/model")
+
 module.exports = {
   get: (req, res, next) => {
-    const text = `RESPONSE ALL USER FROM ${req.path}`
-    res.send(text)
+    const users = User.find({}, (err, users) => {
+      if (err) res.send(err)
+      else res.send(users)
+    })
   },
 
   getOne: (req, res, next) => {
-    const text = `RESPONSE ONE USER FROM ${req.path}`
-    res.send(text)
+    const user = User.findOne(
+      {
+        id: req.params.id
+      },
+      (err, user) => {
+        if (err) res.send(err)
+        else res.send(user)
+      }
+    )
   }
 }
