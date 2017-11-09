aed0ea71bdf54438b5a04a71bf1762e026c4b64d
M Haidar Hanif <git@mhaidarhanif.com>
2017-11-09 16:43:00 +0700

    Implement POST new question

--- api/routes/questions/controller.js
+++ api/routes/questions/controller.js
@@ -1,18 +1,53 @@
 const Question = require("./model")
 
+const helpers = require("../helpers")
+
 module.exports = {
   get: (req, res, next) => {
-    const text = `RESPONSE ALL QUESTIONS FROM ${req.path}`
-    res.send(text)
+    Question.find({}, (err, questions) => {
+      if (err) res.send(err)
+      else res.send(questions)
+    })
   },
 
   getOne: (req, res, next) => {
-    const text = `RESPONSE ONE QUESTIONS FROM ${req.path}`
-    res.send(text)
+    Question.findOne({ id: req.params.id }, (err, question) => {
+      if (err) res.send(err)
+      else res.send(question)
+    })
   },
 
   post: (req, res, next) => {
-    const text = `POST ONE QUESTION`
-    res.send(text)
-  }
+    const token = req.headers.authorization || req.body.token
+    const user = helpers.decodeToken(token)
+
+    if (user) {
+      // PREPARE NEW QUESTION
+      const newQuestion = new Question({
+        title: req.body.title,
+        createdBy: user._id
+      })
+
+      console.log(newQuestion)
+
+      // SAVE THAT NEW QUESTION
+      newQuestion.save(err => {
+        err
+          ? res.send(err)
+          : res.send({
+              message: "New question saved",
+              data: newQuestion
+            })
+      })
+    } else {
+      // NOTIFY IF USER TOKEN IS INVALID
+      res.send({ message: "User token is invalid" })
+    }
+  },
+
+  delete: (req, res, next) => {},
+
+  deleteOne: (req, res, next) => {},
+
+  putOne: (req, res, next) => {}
 }
--- api/routes/questions/index.js
+++ api/routes/questions/index.js
@@ -6,10 +6,11 @@ const controller = require("./controller")
 router.get("/", controller.get)
 router.get("/:id", controller.getOne)
 
-// router.post("/", controller.post)
-//
-// router.delete("/", controller.delete)
-//
-// router.put("/:id", controller.putOne)
+router.post("/", controller.post)
+
+router.delete("/", controller.delete)
+router.delete("/:id", controller.deleteOne)
+
+router.put("/:id", controller.putOne)
 
 module.exports = router
