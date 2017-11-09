cf6d1e477308cdc77ff9c66efe87f60b371d6bea
M Haidar Hanif <git@mhaidarhanif.com>
2017-11-09 18:41:44 +0700

    Start to implement post answer to question

---
+++ api/routes/questions/answers/controller.js
@@ -0,0 +1,51 @@
+const User = require("../../users/model")
+const Question = require("../model")
+
+const helpers = require("../../helpers")
+
+module.exports = {
+  get: (req, res, next) => {
+    res.send("GET ANSWERS")
+  },
+
+  getOne: (req, res, next) => {},
+
+  post: (req, res, next) => {
+    const token = req.headers.authorization || req.body.token
+    const user = helpers.decodeToken(token)
+
+    console.log(user)
+
+    if (user) {
+      // PREPARE NEW QUESTION
+      const newAnswer = {
+        text: req.body.text,
+        createdBy: user._id
+      }
+      console.log(newAnswer)
+
+      // PUSH THAT NEW ANSWER TO THE SELECTED QUESTION
+      Question.update(
+        { id: req.params.id },
+        { $push: { answers: newAnswer } },
+        err => {
+          err
+            ? res.send(err)
+            : res.send({
+                message: "New question saved",
+                data: newAnswer
+              })
+        }
+      )
+    } else {
+      // NOTIFY IF USER TOKEN IS INVALID
+      res.send({ message: "User token is invalid" })
+    }
+  },
+
+  delete: (req, res, next) => {},
+
+  deleteOne: (req, res, next) => {},
+
+  putOne: (req, res, next) => {}
+}
--- api/routes/questions/index.js
+++ api/routes/questions/index.js
@@ -1,16 +1,22 @@
 const express = require("express")
 const router = express.Router()
 
-const controller = require("./controller")
+const question = require("./controller")
 
-router.get("/", controller.get)
-router.get("/:id", controller.getOne)
+router.get("/", question.get)
+router.get("/:id", question.getOne)
+router.post("/", question.post)
+router.delete("/", question.delete)
+router.delete("/:id", question.deleteOne)
+router.put("/:id", question.putOne)
 
-router.post("/", controller.post)
+const answer = require("./answers/controller")
 
-router.delete("/", controller.delete)
-router.delete("/:id", controller.deleteOne)
-
-router.put("/:id", controller.putOne)
+router.get("/questions/:id/answers", answer.get)
+router.get("/questions/:id/answers/:id", answer.getOne)
+router.post("/questions/:id/answers", answer.post)
+router.delete("/questions/:id/answers", answer.delete)
+router.delete("/questions/:id/answers/:id", answer.deleteOne)
+router.put("/questions/:id/answers/:id", answer.putOne)
 
 module.exports = router
