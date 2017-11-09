ee0198f0c0eafb66c5688909e34744a6dc470a5f
M Haidar Hanif <git@mhaidarhanif.com>
2017-11-09 17:21:45 +0700

    Implement AskBar new question to API

--- web/src/organisms/AskBar.js
+++ web/src/organisms/AskBar.js
@@ -1,30 +1,79 @@
 import React from "react"
 import { Button, Form, FormGroup, Input } from "reactstrap"
+import axios from "axios"
 
 import LinkToProfile from "../atoms/LinkToProfile"
 
-const Ask = ({ user }) => {
-  return (
-    <Form>
-      <h6>
-        <LinkToProfile user={user} /> asked
-      </h6>
-      <FormGroup>
-        <Input
-          type="textarea"
-          name="askTextarea"
-          id="askTextarea"
-          placeholder="What is your question?"
-          onKeyDown={e => {
-            if (e.keyCode === 13 && !e.shiftKey) {
-              e.preventDefault()
-            }
-          }}
-        />
-      </FormGroup>
-      <Button color="primary">Ask Question</Button>
-    </Form>
-  )
-}
+import helpers from "../helpers"
 
-export default Ask
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
+axios.defaults.headers.common["Authorization"] = helpers.getToken()
+
+export default class AskBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      askTextarea: ""
+    }
+  }
+
+  handleChange = event => {
+    const name = event.target.name
+    const value = event.target.value
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    const payload = {
+      title: this.state.askTextarea,
+      user: this.props.user
+    }
+
+    axios
+      .post(`/api/questions`, payload)
+      .then(response => {
+        // NOTIFY ASKER
+        console.log(response.data)
+        alert(`Asking success!`)
+        // REDIRECT TO ANSWER
+        this.props.history.push("/answer")
+      })
+      .catch(error => {
+        console.log(error)
+        alert(`${error}`)
+      })
+  }
+
+  render() {
+    const user = this.props.user
+
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <h6>
+          <LinkToProfile user={user} /> asked
+        </h6>
+        <FormGroup>
+          <Input
+            type="textarea"
+            name="askTextarea"
+            id="askTextarea"
+            placeholder="What is your question?"
+            value={this.state.askTextarea}
+            onChange={this.handleChange}
+            onKeyDown={e => {
+              if (e.keyCode === 13 && !e.shiftKey) {
+                e.preventDefault()
+              }
+            }}
+          />
+        </FormGroup>
+        <Button color="primary">Ask Question</Button>
+      </Form>
+    )
+  }
+}
