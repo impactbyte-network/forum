90d127dcb8943eb879b63d15feaf659a6448a018
M Haidar Hanif <git@mhaidarhanif.com>
2017-11-09 17:32:36 +0700

    Prepare to display Question page

--- web/src/pages/Question.js
+++ web/src/pages/Question.js
@@ -1,44 +1,66 @@
 import React from "react"
 import { Col } from "reactstrap"
+import axios from "axios"
 
 import Layout from "../templates/Layout"
 
 import QuestionCard from "../organisms/QuestionCard"
 
-import DATA_QUESTIONS from "../data/questions"
+import helpers from "../helpers"
 
-const Question = props => {
-  const question = DATA_QUESTIONS.filter(question => {
-    return question.id === Number(props.match.params.id)
-  })
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
+axios.defaults.headers.common["Authorization"] = helpers.getToken()
 
-  const answers = question[0].answers
+export default class Question extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: Number(props.match.params.id),
+      question: {},
+      answers: []
+    }
+  }
 
-  return (
-    <Layout>
-      <Col>
-        <QuestionCard question={question[0]} />
+  componentWillMount() {
+    axios
+      .get(`/api/questions/${this.state.id}`)
+      .then(response => {
+        console.log(response.data)
+        this.setState({
+          question: response.data,
+          answers: response.data.answers
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        alert(err)
+      })
+  }
 
-        <hr />
+  render(props) {
+    const question = this.state.question
+    const answers = this.state.answers
 
-        <div>
-          {answers.length ? `${answers.length} answers` : `No answer yet`}
-        </div>
-
-        <hr />
-
-        {answers.map(answer => {
-          return (
-            <div key={answer.id}>
-              <h6>{answer.createdBy.name}</h6>
-              <p>{answer.text}</p>
-              <hr />
-            </div>
-          )
-        })}
-      </Col>
-    </Layout>
-  )
+    return (
+      <Layout>
+        <Col>
+          {/* <QuestionCard question={question} /> */}
+          <hr />
+          <div>
+            {answers.length ? `${answers.length} answers` : `No answer yet`}
+          </div>
+          {/* <hr />
+          {answers.map(answer => {
+            return (
+              <div key={answer.id}>
+                <h6>{answer.createdBy.name}</h6>
+                <p>{answer.text}</p>
+                <hr />
+              </div>
+            )
+          })} */}
+        </Col>
+      </Layout>
+    )
+  }
 }
-
-export default Question
